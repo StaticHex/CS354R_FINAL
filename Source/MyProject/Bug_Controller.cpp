698e0ff39f2b55dfd6844b98e43057cc7468de31
Joseph Bourque <indegon1@gmail.com>
2016-12-05 12:33:14 -0600

    Initial commit

---
+++ Source/MyProject/Bug_Controller.cpp
@@ -0,0 +1,131 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyProject.h"
+#include "Bug_Controller.h"
+
+
+// Sets default values
+ABug_Controller::ABug_Controller()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	newX = 0.f;
+	newY = 0.f;
+
+	oldX.SetNum(Array_size);
+	oldY.SetNum(Array_size);
+
+}
+
+// Called when the game starts or when spawned
+void ABug_Controller::BeginPlay()
+{
+	Super::BeginPlay();
+
+	for (int i = 0; i < Array_size; i++)
+	{
+		newX = generateX(newX);
+		newY = generateX(newY);
+
+		oldX[i] = newX;
+		oldY[i] = newY;
+
+		const UWorld* world = GetWorld();
+
+		if (world) {
+			FActorSpawnParameters spawnParams;
+			spawnParams.Owner = this;
+			spawnParams.Instigator = Instigator;
+
+			FVector newVec;
+			newVec.Set(newX, newY, 10.f);
+
+			int32 num = FMath::RandRange(0, 4);
+
+			if (num == 0)
+				AActor* newActor = GetWorld()->SpawnActor<AProc_bug>(tarantula, newVec, FRotator::ZeroRotator, spawnParams);
+			else if (num == 1) {
+				AActor* newActor = GetWorld()->SpawnActor<AProc_bug>(stagBeetle, newVec, FRotator::ZeroRotator, spawnParams);
+			}
+			else if (num == 2) {
+				AActor* newActor = GetWorld()->SpawnActor<AProc_bug>(butterfly, newVec, FRotator::ZeroRotator, spawnParams);
+			}
+			else if (num == 3) {
+				AActor* newActor = GetWorld()->SpawnActor<AProc_bug>(firefly, newVec, FRotator::ZeroRotator, spawnParams);
+			}
+			else 
+				AActor* newActor = GetWorld()->SpawnActor<AProc_bug>(tree, newVec, FRotator::ZeroRotator, spawnParams);
+
+
+		}
+	}
+	
+}
+
+// Called every frame
+void ABug_Controller::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+float ABug_Controller::generateX(float oldUsedX) {
+	float passBack = 0.f;
+
+	passBack = FMath::RandRange(-10000, 10000);
+
+	passBack = passBack + oldUsedX;
+
+	if (passBack < -10000.f || passBack > 10000.f) {
+		passBack = generateX(oldUsedX);
+	}
+
+	if (!isUsed(oldX, passBack)) {
+		passBack = generateX(oldUsedX);
+	}
+
+	return passBack; 
+}
+
+float ABug_Controller::generateY(float oldUsedY) {
+	float passBack = 0.f;
+
+	passBack = FMath::RandRange(-10000, 10000);
+
+	passBack = passBack + oldUsedY;
+
+	if (passBack < -10000.f || passBack > 10000.f) {
+		passBack = generateY(oldUsedY);
+	}
+
+	if (!isUsed(oldY, passBack)) {
+		passBack = generateY(oldUsedY);
+	}
+
+	return passBack;
+}
+
+bool ABug_Controller::isUsed(TArray<float> &oldArray, float numCheck) {
+	bool flag = true; 
+	for (int i = 0; i < oldArray.Num(); i++)
+		if (distanceBetween(oldArray[i], numCheck) < 50.f)
+			flag = false;
+	return flag;
+}
+
+float ABug_Controller::distanceBetween(float inOne, float inTwo) {
+	float passBack = 0.f;
+	bool flag = isOneGreater(inOne, inTwo);
+	if (flag) passBack = inOne - inTwo;
+	else passBack = inTwo - inOne;
+	return passBack;
+}
+
+bool ABug_Controller::isOneGreater(float inOne, float inTwo) {
+	bool flag = true; 
+	if (inOne <= inTwo)
+		flag = false;
+	return flag;
+}
+
---
+++ Source/MyProject/Bug_Controller.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Proc_bug.h"
+#include "Bug_Controller.generated.h"
+
+UCLASS()
+class MYPROJECT_API ABug_Controller : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABug_Controller();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+private:
+	float generateX(float oldUsedX);
+
+	float generateY(float oldUsedY);
+
+	bool isUsed(TArray<float> &oldArray, float numCheck);
+
+	float distanceBetween(float inOne, float inTwo);
+
+	bool isOneGreater(float inOne, float inTwo);
+
+	TArray<float> oldX;
+
+	TArray<float> oldY;
+
+	float newX;
+
+	float newY;
+
+	const int Array_size = 250;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Tarantula")
+	TSubclassOf<AProc_bug> tarantula;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Stag Beetle")
+	TSubclassOf<AProc_bug> stagBeetle;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Butterfly")
+	TSubclassOf<AProc_bug> butterfly;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	TSubclassOf<AProc_bug> tree;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Fire Fly")
+	TSubclassOf<AProc_bug> firefly;
+	
+};
